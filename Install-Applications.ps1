373d03bce52bbfc2174521ce71f694e86c43d62e
Roman Davydov <60844215+roma-228@users.noreply.github.com>
2023-10-19 16:15:19 +0300

    Update Install-Applications.ps1

--- Install-Applications.ps1
+++ Install-Applications.ps1
@@ -1,16 +1,4 @@
 # Software install Script
-#
-# Applications to install:
-#
-# Foxit Reader Enterprise Packaging (requires registration)
-# https://kb.foxitsoftware.com/hc/en-us/articles/360040658811-Where-to-download-Foxit-Reader-with-Enterprise-Packaging-MSI-
-# 
-# Notepad++
-# https://notepad-plus-plus.org/downloads/v7.8.8/
-# See comments on creating a custom setting to disable auto update message
-# https://community.notepad-plus-plus.org/post/38160
-
-
 
 #region Set logging 
 $logFile = "c:\temp\" + (get-date -format 'yyyyMMdd') + '_softwareinstall.log'
@@ -19,7 +7,8 @@ function Write-Log {
     Write-Output "$(get-date -format 'yyyyMMdd HH:mm:ss') $message" | Out-File -Encoding utf8 $logFile -Append
 }
 #endregion
-#region choco+git
+
+#region Install choco+git
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable -n allowGlobalConfirmation
 choco install git --force --params "/NoAutoCrlf"
@@ -29,35 +18,4 @@ choco install git --force --params "/NoAutoCrlf"
 cd C:\Temp
 wget https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe -O "Docker Desktop Installer.exe"
 Start-Process -Wait -NoNewWindow -FilePath ".\Docker Desktop Installer.exe" -ArgumentList "install --backend=hyper-v --quiet --accept-license " -ErrorAction Stop;
-& $Env:ProgramFiles\Docker\Docker\DockerCli.exe -SwitchDaemon
-#endregion
-
-# Fix for first login delays due to Windows Module Installer
-try {
-    ((Get-Content -path C:\DeprovisioningScript.ps1 -Raw) -replace 'Sysprep.exe /oobe /generalize /quiet /quit', 'Sysprep.exe /oobe /generalize /quit /mode:vm' ) | Set-Content -Path C:\DeprovisioningScript.ps1
-    write-log "Sysprep Mode:VM fix applied"
-}
-catch {
-    $ErrorMessage = $_.Exception.message
-    write-log "Error updating script: $ErrorMessage"
-}
-#endregion
-
-#region Time Zone Redirection
-$Name = "fEnableTimeZoneRedirection"
-$value = "1"
-# Add Registry value
-try {
-    New-ItemProperty -ErrorAction Stop -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Name $name -Value $value -PropertyType DWORD -Force
-    if ((Get-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services").PSObject.Properties.Name -contains $name) {
-        Write-log "Added time zone redirection registry key"
-    }
-    else {
-        write-log "Error locating the Teams registry key"
-    }
-}
-catch {
-    $ErrorMessage = $_.Exception.message
-    write-log "Error adding teams registry KEY: $ErrorMessage"
-}
 #endregion
