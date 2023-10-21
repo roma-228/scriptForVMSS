2217134e6e98f24e74d29ee17d7eb5d5d9def1a7
Roman Davydov <60844215+roma-228@users.noreply.github.com>
2023-10-21 11:30:25 +0300

    Update Install-Applications.ps1

--- Install-Applications.ps1
+++ Install-Applications.ps1
@@ -15,8 +15,6 @@ choco install git --force --params "/NoAutoCrlf"
 #endregion
 
 #region DockerDesktop
-#cd C:\Temp
-#wget https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe -O "Docker Desktop Installer.exe"
 Write-Host 'start process install docker!'
 Start-Process -Wait -NoNewWindow -FilePath "c:\temp\Docker Desktop Installer.exe" -ArgumentList "install --backend=hyper-v --quiet --accept-license " -ErrorAction Stop;
 Write-Host 'finish process install docker!'
