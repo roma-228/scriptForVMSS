2b075b98794c6c8b5427ea51ca356795684f5d1b
Roman Davydov <60844215+roma-228@users.noreply.github.com>
2023-10-23 21:46:10 +0300

    Update StartDocker.ps1

--- StartDocker.ps1
+++ StartDocker.ps1
@@ -1,3 +1,4 @@
 Start-Process -FilePath "C:\\Program Files\\Docker\\Docker\\Docker Desktop.exe"
+Start-Sleep -Seconds 150
 & $Env:ProgramFiles\\Docker\\Docker\\DockerCli.exe -SwitchWindowsEngine
 Set-Service -Name com.docker.service -StartupType Automatic
