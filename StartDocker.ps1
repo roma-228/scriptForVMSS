4b601e21e1413911bd0aae98122a9cede5629fff
Roman Davydov <60844215+roma-228@users.noreply.github.com>
2023-10-25 12:35:29 +0300

    Update StartDocker.ps1

--- StartDocker.ps1
+++ StartDocker.ps1
@@ -1,6 +1,6 @@
 Add-LocalGroupMember -Group "docker-users" -Member "NT AUTHORITY\SYSTEM"
 
-start-service -Name com.docker.service
+
 start "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 
 & $Env:ProgramFiles\Docker\Docker\DockerCli.exe -SwitchWindowsEngine
