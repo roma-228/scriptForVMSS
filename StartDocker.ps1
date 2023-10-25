6a80b7bd369808756bfad7041b9a8e9c2dfbba99
Roman Davydov <60844215+roma-228@users.noreply.github.com>
2023-10-25 13:47:59 +0300

    Update StartDocker.ps1

--- StartDocker.ps1
+++ StartDocker.ps1
@@ -2,7 +2,7 @@ Add-LocalGroupMember -Group "docker-users" -Member "NT AUTHORITY\SYSTEM"
 
 Start-Process -FilePath "C:\\Program Files\\Docker\\Docker\\Docker Desktop.exe"
 C:'\Program Files\'Docker\Docker'\Docker Desktop.exe'
-Start-Sleep -Seconds 300
+Start-Sleep -Seconds 150
 & $Env:ProgramFiles\Docker\Docker\DockerCli.exe -SwitchWindowsEngine
 & C:'\Program Files\'Docker\Docker\DockerCli.exe -SwitchWindowsEngine
 $DockerPath = "C:\Program Files\Docker\Docker\Docker Desktop.exe"
