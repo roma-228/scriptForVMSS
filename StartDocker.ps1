7d74345f55f12b4bb09a962d14fa06524d7b8224
Roman Davydov <60844215+roma-228@users.noreply.github.com>
2023-10-25 16:08:02 +0300

    Add new user

--- StartDocker.ps1
+++ StartDocker.ps1
@@ -1,17 +1,14 @@
 Add-LocalGroupMember -Group "docker-users" -Member "NT AUTHORITY\SYSTEM"
-$Username = 'vmadmin'
-$Password = ConvertTo-SecureString 'Davidovc00375237' -AsPlainText -Force
+
+$Username = 'NewUser1'
+$Password = ConvertTo-SecureString 'Password123' -AsPlainText -Force
 $Credential = New-Object System.Management.Automation.PSCredential($Username, $Password)
+New-LocalUser -Name $Credential.UserName -Password $Credential.Password -FullName 'Full Name' -Description 'Description of the user'
+Add-LocalGroupMember -Group "docker-users" -Member $Username
 $FilePath = "C:\\Program Files\\Docker\\Docker\\Docker Desktop.exe"
 Start-Process -FilePath $FilePath -Credential $Credential
+Start-Process -FilePath $FilePath -Credential $Credential
 & $Env:ProgramFiles\Docker\Docker\DockerCli.exe -SwitchWindowsEngine -Credential $Credential
-
-
-
-$Action = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument "-Command {Start-Process -FilePath 'C:\\Program Files\\Docker\\Docker\\Docker Desktop.exe'; & '$Env:ProgramFiles\\Docker\\Docker\\DockerCli.exe' -SwitchWindowsEngine}"
-$Trigger = New-ScheduledTaskTrigger -AtLogOn
-$Principal = New-ScheduledTaskPrincipal -UserId "AzDevOps" -LogonType ServiceAccount
-Register-ScheduledTask -Action $Action -Trigger $Trigger -TaskName "Run Docker Desktop1" -Description "Start Docker Desktop for AzDevOps" -Principal $Principal
 
 
 Start-Process -FilePath "C:\\Program Files\\Docker\\Docker\\Docker Desktop.exe"
