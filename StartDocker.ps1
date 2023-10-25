969380c112619cd4764016291ad97cb86fff4b87
Roman Davydov <60844215+roma-228@users.noreply.github.com>
2023-10-25 12:02:13 +0300

    add test docker engine

--- StartDocker.ps1
+++ StartDocker.ps1
@@ -4,7 +4,7 @@ start-service -Name com.docker.service
 start "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 
 & $Env:ProgramFiles\Docker\Docker\DockerCli.exe -SwitchWindowsEngine
-
+& C:'\Program Files\'Docker\Docker\DockerCli.exe -SwitchWindowsEngine
 $DockerPath = "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 
 $StartupPath = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\DockerDesctop.lnk"
@@ -12,6 +12,7 @@ echo $DockerPath > "C:\Program Files\output.txt"
 echo $StartupPath >> "C:\Program Files\output.txt"
 whoami | Out-File -FilePath "C:\Program Files\output.txt"
 echo $Env:ProgramFiles\Docker\Docker\DockerCli.exe >> "C:\Program Files\output.txt"
+docker version >> "C:\Program Files\output.txt"
 $WshShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut($StartupPath)
 $Shortcut.TargetPath = $DockerPath
