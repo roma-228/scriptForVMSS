507237d8d9d0ac1b30cbb3c0b3c6e5f0fcfb2752
Roman Davydov <60844215+roma-228@users.noreply.github.com>
2023-10-25 09:38:50 +0300

    Update StartDocker.ps1

--- StartDocker.ps1
+++ StartDocker.ps1
@@ -6,7 +6,7 @@ Start-Process -FilePath "C:\\Program Files\\Docker\\Docker\\Docker Desktop.exe"
 
 $DockerPath = "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 
-$StartupPath = "C:\Users\vmadmin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+$StartupPath = "C:\Users\vmadmin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\DockerDesctop.lnk"
 echo $DockerPath > "C:\Program Files\output.txt"
 echo $StartupPath >> "C:\Program Files\output.txt"
 $WshShell = New-Object -ComObject WScript.Shell
