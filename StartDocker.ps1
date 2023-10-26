88d674a25d3597abbae683904c596b9de7472607
Roman Davydov <60844215+roma-228@users.noreply.github.com>
2023-10-26 12:00:11 +0300

    add data register

--- StartDocker.ps1
+++ StartDocker.ps1
@@ -1,5 +1,10 @@
 Add-LocalGroupMember -Group "docker-users" -Member "NT AUTHORITY\SYSTEM"
 
+$Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+$Name = "Docker Desktop1"
+$Value = "C:\Program Files\Docker\Docker\Docker Desktop.exe -Autostart"
+Set-ItemProperty -Path $Path -Name $Name -Value $Value
+
 $Username = 'NewUser2'
 $Password = ConvertTo-SecureString 'Password123' -AsPlainText -Force
 $Credential = New-Object System.Management.Automation.PSCredential($Username, $Password)
